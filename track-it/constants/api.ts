2fb3cc167b4d7fe787bb10a73e8731806f073460
mangalam-srv <mangalamstp2005@gmail.com>
2026-08-27 16:26:33 +0530

    chore: configure production backend URL

--- track-it/constants/api.ts
+++ track-it/constants/api.ts
@@ -1,5 +1,4 @@
-export const API_BASE_URL = "http://localhost:4000"
-
+export const API_BASE_URL = "https://solvix-backend.onrender.com"
 export function apiUrl(path: string) {
   return `${API_BASE_URL}${path}`
 }
